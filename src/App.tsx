e74d1118cac1fef69115c92fbe6ebf8449c2bdf9
Xaca Rana <and1res@hotmail.com>
2025-04-09 02:06:13 +0000

    add:Ajuste de lectura de datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import About from './pages/about/about'
 import Contacto from "./pages/contact/contacto"
 import './App.css'
 import Home from "./pages/home/home"
+import Galeria from "./pages/gallery/galeria";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Home />
+     <Galeria />
     </>
   )
 }
--- src/pages/gallery/galeria.tsx
+++ src/pages/gallery/galeria.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
 import Menu from "../../components/ui/menu/menu"
+//import axios from 'axios';
+
+async function traerDatos()
+{
+    let url = "https://cataas.com/api/cats"
+    const response = await fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        }).catch((error)=>{
+            return null;
+        });
+    return response.value;
+}
 
 export default function Galeria(){
+    const[datos,setDatos] = useState("");
+    useEffect(()=>{
+        /*axios.get('https://cataas.com/api/cats')
+        .then(function (response) {
+        // handle success
+        console.log(response);
+        //setDatos(response);
+        })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+        })
+        .finally(function () {
+        // always executed
+        });*/
+        setDatos("1,2,3,4");
+        console.log(datos);
+        /*let url = "https://cataas.com/api/cats";
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setDatos(data);
+        });
+        console.log(datos)*/
+    },[]);
+
     return(<>
+    {datos.split(",").map((dato,index)=>{
+        dato
+    })}
     <Menu />
     <h1>Galeria</h1>
     </>);
